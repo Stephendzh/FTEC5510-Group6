c5002bc0ab511c4831a1378046be548d624dcfd9
Stephendzh <90750479+Stephendzh@users.noreply.github.com>
2023-12-15 16:38:48 +0800

    the algorithm files needed
    to use these files and make it able to run you need to fix the os.popen()in the \myapp\views.py

---
+++ a2/1.py
@@ -0,0 +1,147 @@
+import os.path as osp
+import os
+import numpy as np  # linear algebra
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA, TruncatedSVD
+import matplotlib.patches as mpatches
+import time
+
+# Classifier Libraries
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+import collections
+
+# Other Libraries
+from sklearn.model_selection import cross_val_score
+from sklearn.pipeline import make_pipeline
+from imblearn.pipeline import make_pipeline as imbalanced_make_pipeline
+from imblearn.over_sampling import SMOTE
+from imblearn.under_sampling import NearMiss
+from imblearn.metrics import classification_report_imbalanced
+from sklearn.metrics import precision_score, recall_score, f1_score, roc_auc_score, accuracy_score, \
+    classification_report
+from collections import Counter
+from sklearn.model_selection import KFold, StratifiedKFold
+import warnings
+
+warnings.filterwarnings("ignore")
+
+df = pd.read_csv('D:\\courses23fall\\computational_finance\\a2\\creditcard.csv')
+from sklearn.preprocessing import StandardScaler, RobustScaler
+
+std_scaler = StandardScaler()
+rob_scaler = RobustScaler()
+
+df['scaled_amount'] = rob_scaler.fit_transform(df['Amount'].values.reshape(-1, 1))
+df['scaled_time'] = rob_scaler.fit_transform(df['Time'].values.reshape(-1, 1))
+
+df.drop(['Time', 'Amount'], axis=1, inplace=True)
+scaled_amount = df['scaled_amount']
+scaled_time = df['scaled_time']
+
+df.drop(['scaled_amount', 'scaled_time'], axis=1, inplace=True)
+df.insert(0, 'scaled_amount', scaled_amount)
+df.insert(1, 'scaled_time', scaled_time)
+
+X = df.drop('Class', axis=1)
+y = df['Class']
+
+sss = StratifiedKFold(n_splits=5, random_state=None, shuffle=False)
+
+for train_index, test_index in sss.split(X, y):
+    original_Xtrain, original_Xtest = X.iloc[train_index], X.iloc[test_index]
+    original_ytrain, original_ytest = y.iloc[train_index], y.iloc[test_index]
+
+original_Xtrain = original_Xtrain.values
+original_Xtest = original_Xtest.values
+original_ytrain = original_ytrain.values
+original_ytest = original_ytest.values
+
+train_unique_label, train_counts_label = np.unique(original_ytrain, return_counts=True)
+test_unique_label, test_counts_label = np.unique(original_ytest, return_counts=True)
+
+df = df.sample(frac=1)
+
+# amount of fraud classes 492 rows.
+fraud_df = df.loc[df['Class'] == 1]
+non_fraud_df = df.loc[df['Class'] == 0][:492]
+
+normal_distributed_df = pd.concat([fraud_df, non_fraud_df])
+
+# Shuffle dataframe rows
+new_df = normal_distributed_df.sample(frac=1, random_state=42)
+
+v14_fraud = new_df['V14'].loc[new_df['Class'] == 1].values
+q25, q75 = np.percentile(v14_fraud, 25), np.percentile(v14_fraud, 75)
+v14_iqr = q75 - q25
+v14_cut_off = v14_iqr * 1.5
+v14_lower, v14_upper = q25 - v14_cut_off, q75 + v14_cut_off
+new_df = new_df.drop(new_df[(new_df['V14'] > v14_upper) | (new_df['V14'] < v14_lower)].index)
+
+v12_fraud = new_df['V12'].loc[new_df['Class'] == 1].values
+q25, q75 = np.percentile(v12_fraud, 25), np.percentile(v12_fraud, 75)
+v12_iqr = q75 - q25
+v12_cut_off = v12_iqr * 1.5
+v12_lower, v12_upper = q25 - v12_cut_off, q75 + v12_cut_off
+new_df = new_df.drop(new_df[(new_df['V12'] > v12_upper) | (new_df['V12'] < v12_lower)].index)
+
+v10_fraud = new_df['V10'].loc[new_df['Class'] == 1].values
+q25, q75 = np.percentile(v10_fraud, 25), np.percentile(v10_fraud, 75)
+v10_iqr = q75 - q25
+v10_cut_off = v10_iqr * 1.5
+v10_lower, v10_upper = q25 - v10_cut_off, q75 + v10_cut_off
+new_df = new_df.drop(new_df[(new_df['V10'] > v10_upper) | (new_df['V10'] < v10_lower)].index)
+
+X = new_df.drop('Class', axis=1)
+y = new_df['Class']
+from sklearn.model_selection import train_test_split
+
+# This is explicitly used for undersampling.
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+X_train = X_train.values
+X_test = X_test.values
+y_train = y_train.values
+y_test = y_test.values
+
+# %%
+classifiers = {
+    "LogisiticRegression": LogisticRegression(),
+    "KNearest": KNeighborsClassifier(),
+    "Support Vector Classifier": SVC(),
+    "DecisionTreeClassifier": DecisionTreeClassifier()
+}
+
+from sklearn.model_selection import GridSearchCV
+
+# Logistic Regression
+log_reg_params = {"penalty": ['l1', 'l2'], 'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000]}
+
+grid_log_reg = GridSearchCV(LogisticRegression(), log_reg_params)
+grid_log_reg.fit(X_train, y_train)
+# We automatically get the logistic regression with the best parameters.
+log_reg = grid_log_reg.best_estimator_
+
+
+pair = []
+for i in range(30):
+    pair.append((X_train[:,i].mean(), X_train[:,i].std()))
+def data_synthetic():
+    test_list = []
+    for i in pair:
+        test_list.append(np.random.normal(i[0]/10,i[1]/5))
+    return np.array(test_list)
+
+data = np.zeros(shape=(30,40))
+for i in range(40):
+    data[:,i] = data_synthetic()
+
+print(log_reg.predict(np.array(data.transpose())).mean())
+
---
+++ a2/hello.py
@@ -0,0 +1,2 @@
+import os
+print(os.popen('python D:\\courses23fall\\computational_finance\\a2\\gcn.py').read())
